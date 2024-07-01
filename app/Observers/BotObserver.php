da8862f923c9af108b4de8fa109fef56d63134cb
phuongnam15 <ppnam150399@gmail.com>
2024-07-01 10:45:01 +0700

    update

--- app/Observers/BotObserver.php
+++ app/Observers/BotObserver.php
@@ -3,13 +3,21 @@
 namespace App\Observers;
 
 use App\Models\Bot;
+use App\Services\BotService\BotService;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Cache;
 
 class BotObserver
 {
+    protected $botService;
+    public function __construct(BotService $botService)
+    {
+        $this->botService = $botService;
+    }
     public function updated(Bot $bot)
     {
+        $this->botService->disableWebhook($bot->token);
+
         if ($bot->status == Bot::STATUS_ACTIVE) {
 
             Cache::put('active_bot_token', $bot->token);
