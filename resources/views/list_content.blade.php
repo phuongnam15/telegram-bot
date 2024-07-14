c37aceeef0dee93e40ec825863fd1c4f34c3e2e3
phuongnam15 <ppnam150399@gmail.com>
2024-07-14 22:44:06 +0700

    update

--- resources/views/list_content.blade.php
+++ resources/views/list_content.blade.php
@@ -588,14 +588,21 @@
         });
     </script>
     <script>
-        document.addEventListener('keydown', function(event) {
-            if (event.key === 'F12') {
-                event.preventDefault();
+        (function() {
+            const devtools = /./;
+            devtools.toString = function() {
+                this.opened = true;
+                showWarning();
+            };
+
+            const showWarning = () => {
                 console.clear();
                 console.log('%cDừng lại!', 'color: red; font-size: 42px; font-weight: bold;');
                 console.log('%cTraffic thật không tool bảo hành vĩnh viễn, bao top, admin group chợ nghiện seo vui lòng liên hệ telegram:https://t.me/chonghienseo', 'font-size: 16px;');
-            }
-        });
+            };
+
+            console.log('%c', devtools);
+        })();
     </script>
 </body>
 
