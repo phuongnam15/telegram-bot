1ccfe0d8111a7311eeb87548217530ba9f6f44f8
phuongnam15 <ppnam150399@gmail.com>
2024-07-31 09:01:07 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -117,7 +117,7 @@
                                 'access_token',
                                 data?.data?.token,
                             );
-                            window.location.href = '/bot';
+                            window.location.href = '/';
                         } else {
                             const data = await response.json();
                             errorMessageElement.textContent = data.message;
