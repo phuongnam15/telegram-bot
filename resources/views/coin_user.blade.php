f01fd22d614b6cb1d4f133ec6d737ebb8961a16f
phuongnam15 <ppnam150399@gmail.com>
2024-08-20 09:26:10 +0700

    update

--- resources/views/coin_user.blade.php
+++ resources/views/coin_user.blade.php
@@ -4,8 +4,8 @@
 
 @section("content")
 <div class="container mt-4">
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400" id="listUserTable">
-        <thead class="text-xs text-gray-700 uppercase bg-[#2a2d35] dark:text-gray-400">
+    <table class="w-full text-sm text-left rtl:text-right" id="listUserTable">
+        <thead class="text-xs text-gray-300 uppercase bg-[#2a2d35]">
             <tr>
                 <th scope="col" class="px-6 py-3">
                     Name
@@ -104,11 +104,11 @@
 
             data.forEach(user => {
                 $('#listUserTable tbody').append(`
-                    <tr class="bg-[#1e2026] border-b dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                    <tr class="bg-[#1e2026] border-b border-gray-700 hover:bg-gray-900 text-gray-400">
+                        <th scope="row" class="flex items-center px-6 py-4 whitespace-nowrap">
                             <img class="w-10 h-10 rounded-full" src="${user.avatar}" alt="Jese image">
                             <div class="ps-3">
-                                <div class="text-base font-semibold">${user.firstname + user.lastname}</div>
+                                <div class="font-semibold">${user.firstname + user.lastname}</div>
                                 <div class="font-normal text-gray-500">@${user.username}</div>
                             </div>
                         </th>
